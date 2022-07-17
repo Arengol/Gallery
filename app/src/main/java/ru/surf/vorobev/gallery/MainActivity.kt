a5bbacfb8a8810457ebd18105100ce7df04485e0
Vladimir Vorobev <worobev2001@gmail.com>
2022-07-17 13:19:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/surf/vorobev/gallery/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.surf.vorobev.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
